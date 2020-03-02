58ae3c23d0a7c31ea47f470d1944f814f587e76b
Mathhead200 <chris@mathhead200.com>
2020-03-02 15:48:52 -0500

    v5.0.0 some test data is in

---
+++ MSD Research Project (v6.0.0), 03-02-2020/eclipse-workspace/MSD Graph Model/src/Particle.java
@@ -0,0 +1,10 @@
+
+/**
+ * Represents a node of the graph, i.e. a particle in the device. For example,
+ * this might be an atom, or a free electron.
+ * 
+ * @author Christopher D'Angelo
+ */
+public interface Particle {
+	
+}
